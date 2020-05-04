6fb9b9c5374a8ae2ef7ad6962e4c5d836053a544
zhs61 <55224931+zhs61@users.noreply.github.com>
2020-05-04 01:10:16 -0400

    push conponents

---
+++ src/components/AddAttribute.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <el-form :inline="true" :model="newattribute" class="form-inline">
+            <el-form-item label="Name">
+                <el-input v-model="newattribute.name" placeholder="name"></el-input>
+            </el-form-item>
+            <el-form-item label="Type">
+                <el-input v-model="newattribute.type" placeholder="type"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="addAtt">Add</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "AddAttribute",
+    data() {
+      return {
+        newattribute: {
+          name: '',
+          type: ''
+        }
+      }
+    },
+    methods: {
+      addAtt(e) {
+        e.preventDefault();
+        console.log(this.newattribute.name);
+        // Send up to parent
+        this.$emit('add-att', this.newattribute);
+
+        this.title = '';
+      }
+    }
+  }
+</script>
---
+++ src/components/ModelList.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+        <el-table-column
+        prop="name"
+        label="Attribute"
+        >
+        </el-table-column>
+        <el-table-column
+        prop="type"
+        label="type"
+        >
+        </el-table-column>
+    </el-table>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    props: ["tableData"]
+  }
+</script>
+
